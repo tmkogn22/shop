f81e24ed58224c8f459d8f51c31d860250ccf389
takogonia <timurkogoniia2205@gmail.com>
2025-03-14 17:37:09 +0300

    add admin functions

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from catalog.models import (Category, Brand, Discount, Promo, Product, ProductImage, Cashback, Cart, Order,
+                            OrderProductsM2M)
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('article', 'brand', 'name', 'price')
+    search_fields = ('article', 'name', 'category__name')
+
+
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Discount)
+admin.site.register(Promo)
+admin.site.register(ProductImage)
+admin.site.register(Cashback)
+admin.site.register(Order)
+admin.site.register(Product, ProductAdmin)
+
